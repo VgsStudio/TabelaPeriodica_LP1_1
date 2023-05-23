0ec8ed1736172f27c6c2eb71dab684fef0d57a28
Vitor Soller <vgsoller@gmail.com>
2023-05-23 12:26:23 -0300

    add TestPeriodicTableScreen 😀

---
+++ src/tests/TestPeriodicTableScreen.java
@@ -0,0 +1,9 @@
+package tests;
+
+import main.ui.PeriodicTableScreen;
+
+public class TestPeriodicTableScreen {
+    public static void main(String[] args) {
+        new PeriodicTableScreen();
+    }
+}
