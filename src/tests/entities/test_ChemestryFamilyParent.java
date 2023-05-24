07089582efae36cab228f8bb726935e6790691d9
João Vitor <21.01075-7@maua.br>
2023-05-24 10:03:00 -0300

    Adicionando novas entidades (quebrado)

--- src/tests/entities/test_ChemestryFamilyParent.java
+++ src/tests/entities/test_ChemestryFamilyParent.java
@@ -1,8 +1,9 @@
+package tests.entities;
 import main.entities.ChemestryFamilyParent;
 
-public class test_ChemestryFamilyParent {
+public class Test_ChemestryFamilyParent {
     public static void main(String[] args) {
-        System.out.println("test_ChemestryFamilyParent.java");
+        System.out.println("Test_ChemestryFamilyParent.java");
         ChemestryFamilyParent cfp = new ChemestryFamilyParent();
         System.out.println(cfp.getName() == "Metais Alcalinos");
         System.out.println(cfp.getColor() == "Red");
