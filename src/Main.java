7d5f31ab482a64dd7a250bdb675d51f098b42ee0
Vitor Soller <vgsoller@gmail.com>
2023-05-27 15:44:14 -0300

    change to sql repo 🐔

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import main.back.domain.repo.RepositoryInterface;
 import main.back.modules.GetAllElements.GetAllElementsPresenter;
 import main.back.modules.UpdateInfo.UpdateInfoPresenter;
 import main.back.shared.repo.RepositoryMock;
+import main.back.shared.repo.RepositoryMySQL;
 import main.ui.more_Info.MoreInfoPopUpFactory;
 import main.ui.table.PeriodicTable;
 import main.ui.table.PeriodicTableScreen;
@@ -9,7 +10,7 @@ import main.ui.table.PeriodicTableScreen;
 public class Main {
 
     public static void main(String[] args) {
-        RepositoryInterface repo = new RepositoryMock(); // TODO change to RepositoryMySQL
+        RepositoryInterface repo = new RepositoryMySQL();
         GetAllElementsPresenter getAllElementsPresenter = new GetAllElementsPresenter(repo);
         UpdateInfoPresenter updateInfoPresenter = new UpdateInfoPresenter(repo);
         MoreInfoPopUpFactory moreInfoPopUpFactory = new MoreInfoPopUpFactory(updateInfoPresenter);
